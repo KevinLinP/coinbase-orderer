1f946d19f72955762d549618826aab3fb7840b12
Kevin Lin <kevin.lin.p@gmail.com>
2021-07-12 21:24:05 -0400

    fin version 0.1

--- order.js
+++ order.js
@@ -2,13 +2,11 @@
 
 const _ = require('lodash')
 const readline = require('readline');
+const axios = require('axios')
+const crypto = require('crypto')
 
 const MIN_BUY = 5
-
-// const axios = require('axios')
-// const crypto = require('crypto')
-
-// console.log(process.argv)
+const THROTTLE_MS = 5000
 
 const calculateBuys = () => {
   const amount = parseInt(process.argv[2])
@@ -27,7 +25,37 @@ const calculateBuys = () => {
   return buys
 }
 
-const main = () => {
+const executeBuys = async (buys) => {
+  const price = _.first(buys)
+  const now = new Date()
+  const size = 1.0 * MIN_BUY / price
+
+  const body = {
+    client_oid: `BTCUSD_${Math.floor(now / 1000)}`,
+    type: 'limit',
+    side: 'buy',
+    product_id: 'BTC-USD',
+    price,
+    size: size.toFixed(8)
+  }
+
+  const response = await CoinbasePro.post({
+    path: `/orders`,
+    body
+  })
+
+  console.log(response)
+
+  const remaining = _.tail(buys)
+
+  if (remaining.length > 0) {
+    setTimeout(() => {
+      executeBuys(remaining)
+    }, THROTTLE_MS)
+  }
+}
+
+const main = async () => {
   const buys = calculateBuys()
 
   console.log(buys)
@@ -40,6 +68,7 @@ const main = () => {
   rl.question('type EXECUTE to execute\r\n', (answer) => {
     if (answer === 'EXECUTE') {
       console.log('executing ...')
+      executeBuys(buys)
     }
 
     rl.close()
@@ -48,118 +77,55 @@ const main = () => {
 
 main()
 
-// module.exports.get = async (event, context) => {
-//   const response = await CoinbasePro.get({path: `/accounts`})
-//   console.log(response)
-// };
+class CoinbasePro {
+  static baseUri = 'https://api.pro.coinbase.com'
 
-// module.exports.buy = async (event, context) => {
-//   const now = new Date()
-//   const accountResponse = await CoinbasePro.get({path: `/accounts/${process.env.CBP_USD_ACCOUNT}`})
-//   const availableUsd = parseFloat(accountResponse.available)
-//   const buyBtcAmount = parseFloat(process.env.BUY_BTC_AMOUNT)
-//   let buyResponse = null
+  static async get({path}) {
+    const headers = this.generateHeaders({
+      method: 'GET',
+      path,
+    })
 
-//   if (availableUsd > buyBtcAmount) {
-//     const body = {
-//       client_oid: `BTCUSD_${Math.floor(now / 1000)}`,
-//       type: 'market',
-//       side: 'buy',
-//       product_id: 'BTC-USD',
-//       funds: buyBtcAmount,
-//     }
+    const response = await axios.get(
+      this.baseUri + path,
+      {headers}
+    );
 
-//     buyResponse = await CoinbasePro.post({
-//       path: `/orders`,
-//       body
-//     })
-//   }
+    return response.data
+  }
 
-//   console.log(JSON.stringify({
-//     availableUsd,
-//     buyBtcAmount,
-//     now: now.toISOString(),
-//     buyResponse
-//   }))
-// };
+  static async post({path, body}) {
+    const headers = this.generateHeaders({
+      method: 'POST',
+      path,
+      body,
+    })
 
-// module.exports.deposit = async (event, context) => {
-//   const time = new Date();
-//   console.log(`deposit ${time}`);
-// };
+    const response = await axios.post(
+      this.baseUri + path, 
+      body,
+      {headers}
+    );
 
-// class CoinbasePro {
-//   static baseUri = 'https://api.pro.coinbase.com'
+    return response.data
+  }
 
-//   static async get({path}) {
-//     const headers = this.generateHeaders({
-//       method: 'GET',
-//       path,
-//     })
+  static generateHeaders({method, path, body}) {
+    const timestamp = Math.floor(Date.now() / 1000)
+    const bodyStr = body ? JSON.stringify(body) : '';
 
-//     const response = await axios.get(
-//       this.baseUri + path,
-//       {headers}
-//     );
+    const what = timestamp + method + path + bodyStr
+    const key = Buffer.from(process.env.CBP_SECRET, 'base64');
+    const hmac = crypto.createHmac('sha256', key);
+    const signature = hmac.update(what).digest('base64');
 
-//     return response.data
-//   }
+    return {
+      'CB-ACCESS-KEY': process.env.CBP_KEY,
+      'CB-ACCESS-TIMESTAMP': timestamp,
+      'CB-ACCESS-SIGN': signature,
+      'CB-ACCESS-PASSPHRASE': process.env.CBP_PASSPHRASE,
+    }
+  }
 
-//   // static async deposit({amount}) {
-//   //   this.post({
-//   //     path: '/deposits/payment-methods',
-//   //     body: {
-//   //       amount: amount.toString(),
-//   //       currency: 'USD',
-//   //       payment_method: process.env.CBP_PAYMENT_METHOD_ID,
-//   //     }
-//   //   })
-//   // }
-
-//   // static async transfer({amount}) {
-//   //   this.post({
-//   //     path: '/profiles/transfer',
-//   //     body: {
-//   //       from: process.env.CBP_DEFAULT_PROFILE,
-//   //       to: process.env.CBP_TRADING_PROFILE,
-//   //       amount: amount.toString(),
-//   //       currency: 'USD',
-//   //     }
-//   //   })
-//   // }
-
-//   static async post({path, body}) {
-//     const headers = this.generateHeaders({
-//       method: 'POST',
-//       path,
-//       body,
-//     })
-
-//     const response = await axios.post(
-//       this.baseUri + path, 
-//       body,
-//       {headers}
-//     );
-
-//     return response.data
-//   }
-
-//   static generateHeaders({method, path, body}) {
-//     const timestamp = Math.floor(Date.now() / 1000)
-//     const bodyStr = body ? JSON.stringify(body) : '';
-
-//     const what = timestamp + method + path + bodyStr
-//     const key = Buffer.from(process.env.CBP_SECRET, 'base64');
-//     const hmac = crypto.createHmac('sha256', key);
-//     const signature = hmac.update(what).digest('base64');
-
-//     return {
-//       'CB-ACCESS-KEY': process.env.CBP_KEY,
-//       'CB-ACCESS-TIMESTAMP': timestamp,
-//       'CB-ACCESS-SIGN': signature,
-//       'CB-ACCESS-PASSPHRASE': process.env.CBP_PASSPHRASE,
-//     }
-//   }
-
-// }
+}
 
